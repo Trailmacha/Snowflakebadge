eaf2fc8975ab004f40bbcdaf810d99717d9d9684
Trailmacha <111961402+Trailmacha@users.noreply.github.com>
2022-08-24 13:32:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas
 streamlit.title('My Datais here!')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe('my_fruit_list')
+streamlit.dataframe(my_fruit_list)
